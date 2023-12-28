8b23536cb3d3a9ddfb89b3cfa5052938ae7e7e80
Helium7 <helium747@gmail.com>
2023-12-28 12:08:05 +0800

    fix: rate limit for different ip

--- src/main/kotlin/team/bupt/h7/plugins/RateLimit.kt
+++ src/main/kotlin/team/bupt/h7/plugins/RateLimit.kt
@@ -3,6 +3,7 @@ package team.bupt.h7.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.plugins.ratelimit.*
+import io.ktor.server.request.*
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRateLimit() {
@@ -10,7 +11,7 @@ fun Application.configureRateLimit() {
         global {
             rateLimiter(limit = 200, refillPeriod = 60.seconds)
             requestKey {
-                it.request.origin.remoteHost
+                it.request.header("CF-Connecting-IP") ?: it.request.origin.remoteHost
             }
         }
     }
