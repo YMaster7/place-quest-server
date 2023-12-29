a653f2276a01fa9ff95987d07a29d70cd8558e3b
Helium7 <helium747@gmail.com>
2023-12-29 10:29:47 +0800

    feat: deal stats filtered by destination type

--- src/main/kotlin/team/bupt/h7/models/responses/SeekPlaceDealStatisticsResponse.kt
+++ src/main/kotlin/team/bupt/h7/models/responses/SeekPlaceDealStatisticsResponse.kt
@@ -6,12 +6,16 @@ import team.bupt.h7.models.entities.SeekPlaceDealStatistic
 @Serializable
 data class SeekPlaceDealStatisticsResponse(
     val yearMonth: String,
+    val region: String,
+    val destinationType: String,
     val totalDeals: Int,
     val totalBrokerage: Int,
 )
 
 fun SeekPlaceDealStatistic.toResponse() = SeekPlaceDealStatisticsResponse(
     yearMonth = yearMonth,
+    region = region,
+    destinationType = destinationType,
     totalDeals = totalDeals,
     totalBrokerage = totalBrokerage,
 )
